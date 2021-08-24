f71cd6630469b7575ac02b39cf1be0ba3a746fa6
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-08-24 18:25:32 +0300

    added opencv and imagemagick packages

--- isp.sh
+++ isp.sh
@@ -8,25 +8,25 @@ sudo add-apt-repository ppa:alex-p/tesseract-ocr #installing ocr packages
 echo "Finished installing requirements for tesseract-ocr ...!"
 sleep 5
 
-sudo apt-get update -y #updating sys
+sudo apt update -y #updating sys
 echo "Finished updating sys!"
 sleep 5
 
-sudo apt install tesseract-ocr #installing tesseract-ocr
+sudo apt install tesseract-ocr -y #installing tesseract-ocr
 echo "Finished installing tesseract-ocr!"
 sleep 5
 
-sudo apt install libtesseract-dev #installing liberaries
+sudo apt install libtesseract-dev -y #installing liberaries
 echo "Finished installing dev packages for tesseract-ocr"
 sleep 5
 
-sudo apt install tesseract-ocr-ara #installing arabic language
-echo "Finished istalling arabic language package!"
+sudo apt install tesseract-ocr-ara -y #installing arabic language
+echo "Finished installing arabic language package!"
 sleep 5
 
-sudo apt install python3-opencv
-echo "Finished installing opencv package"
+sudo apt install python3-opencv -y && sudo apt install imagemagick -y && sudo apt install libmagickwand-dev -y
+echo "Finished installing opencv and imagemagick package"
 sleep 5
 
-sudo apt autoremove #removing un-wanted packages
+sudo apt autoremove -y #removing un-wanted packages
 echo "Finished removing!"
